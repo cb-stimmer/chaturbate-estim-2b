3c5d4d1caeaa7c87cf2c00ab74dfdb46b3ef2f2a
Paul Allen <paulallen87555@gmail.com>
2017-06-20 00:30:56 -0400

    updates for new chaturbate-controller module

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const colors = require('colors/safe');
 const ChaturbateBrowser = require('@paulallen87/chaturbate-browser');
-const ChaturbateEvents = require('@paulallen87/chaturbate-events');
+const ChaturbateController = require('@paulallen87/chaturbate-controller');
 
 const browser = new ChaturbateBrowser();
-const events = new ChaturbateEvents(browser);
+const controller = new ChaturbateController(browser);
 
 const close = (e) => {
   if (e) console.error(e);
@@ -48,125 +48,127 @@ process.on('exit', () => close(null));
 process.on('SIGTERM', () => close(null));
 process.on('uncaughtException', (e) => close(e));
 
-events.on('app_error_log', (e) => {
+controller.on('state_change', (e) => {
+  if (e == 'SOCKET_OPEN') {
+    console.log(colors.black(colors.bgGreen(`Welcome to ${controller.room}'s room`)));  
+  }
+});
+
+controller.on('app_error_log', (e) => {
   console.log(colors.white(colors.bgRed(`ERROR: ${e.message}`)));  
 });
 
-events.on('app_notice', (e) => {
+controller.on('app_notice', (e) => {
   e.messages.forEach((message) => {
     console.log(colors.black(colors.bgCyan(`Notice: ${message}`)));  
   })
 });
 
-events.on('app_tab_refresh', () => {
+controller.on('app_tab_refresh', () => {
   console.log(colors.grey('* tab refreshed'));  
 });
 
-events.on('away_mode_cancel', () => {
+controller.on('away_mode_cancel', () => {
   console.log(colors.grey('* away mode canceled'));  
 });
 
-events.on('clear_app', () => {
+controller.on('clear_app', () => {
   console.log(colors.grey('* clear app'));  
 });
 
-events.on('group_show_approve', (e) => {
+controller.on('group_show_approve', (e) => {
   console.log(colors.grey(`* group show approved for ${e.tokensPerMinute} tokens per minute`));  
 });
 
-events.on('group_show_cancel', () => {
+controller.on('group_show_cancel', () => {
   console.log(colors.grey(`* group show canceled`));  
 });
 
-events.on('group_show_request', (e) => {
+controller.on('group_show_request', (e) => {
   console.log(colors.grey(`* group show request ${e.usersWaiting}/${e.usersRequired} for ${e.tokensPerMinute} tokens per minute`));  
 });
 
-events.on('hidden_show_approve', (e) => {
+controller.on('hidden_show_approve', (e) => {
   console.log(colors.grey(`* hidden show approved, hidden is ${e.initialHideCam}`));  
 });
 
-events.on('kick', (e) => {
+controller.on('kick', (e) => {
   console.log(`${e.username} was kicked`); 
 });
 
-events.on('leave_private_room', (e) => {
+controller.on('leave_private_room', (e) => {
   console.log(colors.grey(`* ${username} has left the private show`));  
 });
 
-events.on('log', (e) => {
+controller.on('log', (e) => {
   console.log(colors.grey(`LOG [${e.message}]`));  
 });
 
-events.on('message_change_request', (e) => {
+controller.on('message_change_request', (e) => {
   console.log(colors.grey(`* message '${e.subject}' has been changed`)); 
 });
 
-events.on('personally_kicked', (e) => {
+controller.on('personally_kicked', (e) => {
   console.log(colors.grey(`* you have been kicked because '${e.reason}'`)); 
 });
 
-events.on('private_message', (e) => {
+controller.on('private_message', (e) => {
   const username = getUserColor(e.user, e.user.username);
   console.log(`[PRIVATE MESSAGE] ${username}: ${e.message}`);  
 });
 
-events.on('private_show_approve', (e) => {
+controller.on('private_show_approve', (e) => {
   console.log(colors.grey(`* private show approved for ${e.tokensPerMinute} tokens per minute`)); 
 });
 
-events.on('private_show_cancel', (e) => {
+controller.on('private_show_cancel', (e) => {
   console.log(colors.grey(`* private show canceled`)); 
 });
 
-events.on('private_show_request', (e) => {
+controller.on('private_show_request', (e) => {
   console.log(colors.grey(`* private show requested by '${e.requesterUsername}' for ${e.tokensPerMinute} tokens per minute`)); 
 });
 
-events.on('promotion', (e) => {
+controller.on('promotion', (e) => {
   console.log(colors.grey(`* '${e.toNick}' has been promoted to moderator by ${e.fromNick}`)); 
 });
 
-events.on('promotion', (e) => {
-  console.log(colors.grey(`* '${e.toNick}' has been promoted to moderator by ${e.fromNick}`)); 
-});
-
-events.on('purchase', (e) => {
+controller.on('purchase', (e) => {
   console.log(colors.white(colors.bgMagenta(e.message))); 
 });
 
-events.on('receive_tip', (e) => {
+controller.on('receive_tip', (e) => {
   console.log(colors.bgYellow(colors.black(`${e.fromUsername} tipped ${e.amount} tokens: ${e.message}`)));   
 });
 
-events.on('refresh_panel', (e) => {
+controller.on('refresh_panel', () => {
   console.log(colors.grey(`* panel refreshed`)); 
 });
 
-events.on('revoke', (e) => {
+controller.on('revoke', (e) => {
   console.log(colors.grey(`* '${e.fromNick}' has revoked moderator privs from ${e.toNick}`)); 
 });
 
-events.on('room_count', (e) => {
+controller.on('room_count', (e) => {
   console.log(colors.black(colors.bgWhite(`${e.count} viewers`))); 
 });
 
-events.on('room_entry', (e) => {
+controller.on('room_entry', (e) => {
   const username = getUserColor(e.user, e.user.username);
   console.log(`${username} has joined the room`);  
 });
 
-events.on('room_leave', (e) => {
+controller.on('room_leave', (e) => {
   const username = getUserColor(e.user, e.user.username);
   console.log(`${username} has left the room`);  
 });
 
-events.on('room_message', (e) => {
+controller.on('room_message', (e) => {
   const username = getUserColor(e.user, e.user.username);
   console.log(`${username}: ${e.message}`);  
 });
 
-events.on('settings_update', (e) => {
+controller.on('settings_update', (e) => {
   console.log(colors.grey(`* Allow Privates: ${e.allowPrivates}`)); 
   console.log(colors.grey(`* Allow Groups: ${e.allowGroups}`)); 
   console.log(colors.grey(`* Min Users For Groups: ${e.minimumUsersForGroupShow}`)); 
@@ -175,19 +177,19 @@ events.on('settings_update', (e) => {
   console.log(colors.grey(`* Spy Price: ${e.spyPrice}`)); 
 });
 
-events.on('silence', (e) => {
+controller.on('silence', (e) => {
   console.log(`${e.silencedNick} was silenced by ${e.silencerNick}`);
 });
 
-events.on('tip', (e) => {
+controller.on('tip', (e) => {
   console.log(colors.bgYellow(colors.black(`${e.user.username} tipped ${e.amount} tokens`)));   
 });
 
-events.on('title_change', (e) => {
+controller.on('title_change', (e) => {
   console.log(colors.white(colors.bgBlue(`room title changed to: ${e.title}`)));  
 });
 
-events.on('token_balance_update', (e) => {
+controller.on('token_balance_update', (e) => {
   console.log(colors.grey(`* token balances: ${e.usernames} -> ${e.tokenAmounts}`));   
 });
 
